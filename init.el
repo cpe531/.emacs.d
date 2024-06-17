83f908b2baf0da90d9033f7cb432d3e803cc4241
Christian Erwin <cpe531@nmsu.edu>
2024-06-17 13:59:53 -0600

    First commit.

---
+++ init.el
@@ -0,0 +1,57 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("" default))
+ '(package-selected-packages '(catppuccin-theme fireplace))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; My stuff
+(require 'package)
+;;(setq package-enable-at-startup nil)	;; from Uncle Dave Emacs Tutorial
+;; C-h v -- look up variables (user-init-file)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; colour theme
+;; M-x load-theme is the correct way to do this
+;;(load-theme 'deeper-blue)	;; best theme -- Capoera Jungle Science Man in Yokohama, Surf's Up!
+(load-theme 'catppuccin :no-confirm)
+
+;; font
+;;(set-face-attribute 'default nil :font "IBM Plex Mono")
+(set-face-attribute 'default nil :font "Iosevka Term")
+
+;; stop yelling at me
+(setq visible-bell t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Shell
+;;(setq-default explicit-shell-file-name "/Users/chris/scoop/apps/git/current/bin/bash.exe")
+
+;; Basic Config
+(setq next-screen-context-lines 5)	;; Number of lines of continuity when scrolling by screenfuls.
+
+;; Programming
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+;; C/C++
+(add-hook 'c-mode-hook 'display-fill-column-indicator-mode)
+(add-hook 'c++-mode-hook 'display-fill-column-indicator-mode)
+;;(setq display-fill-column-indicator-column 80)
+(defun erwin-c-setup ()
+  (setq c-default-style "stroustrup" c-basic-offset 4
+	fill-column 80))
+(add-hook 'c-mode-hook 'erwin-c-setup)
+(add-hook 'c++-mode-hook 'erwin-c-setup)
+;;(setq c-default-style "stroustrup" c-basic-offset 4)
